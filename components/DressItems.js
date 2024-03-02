7d6d7964091b3c56e2b9ca6824951521ed023041
Kashif <ahmed.kashif1001@gmail.com>
2024-03-02 23:32:17 +0500

    pricing and add pressable

--- components/DressItems.js
+++ components/DressItems.js
@@ -1,14 +1,51 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, Image, Pressable } from "react-native";
+import React from "react";
 
-const DressItems = () => {
+const DressItems = ({ item }) => {
   return (
     <View>
-      <Text>DressItems</Text>
+      <Pressable style={styles.firstPressable}>
+        <View>
+          <Image
+            style={{ width: 70, height: 70 }}
+            source={{ uri: item.image }}
+          />
+        </View>
+
+        <View>
+          <Text>{item.name}</Text>
+          <Text>{item.price}</Text>
+        </View>
+
+        <Pressable style={{ width: 80 }}>
+          <Text
+            style={styles.secondPressable}>
+            Add
+          </Text>
+        </Pressable>
+      </Pressable>
     </View>
-  )
-}
+  );
+};
 
-export default DressItems
+export default DressItems;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  firstPressable: {
+    backgroundColor: "#ADD8E6",
+    borderRadius: 8,
+    padding: 10,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    margin: 14,
+  },
+  secondPressable: {
+    borderColor: "gray",
+    borderWidth: 0.8,
+    marginVertical: 10,
+    color: "#088F8F",
+    textAlign: "center",
+    padding: 5,
+  },
+});
