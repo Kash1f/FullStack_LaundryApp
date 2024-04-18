21b965a2eca7a727ec2164e4eab20b8b40aa29a8
Kashif <ahmed.kashif1001@gmail.com>
2024-04-18 23:54:39 +0500

    reducers defined, add remove increment decrement

--- CartReducer.js
+++ CartReducer.js
@@ -21,14 +21,41 @@ export const CartSlice = createSlice({
             }},
             removeFromCart:(state,action) =>
             
-            //filter method will be used here to filter the items in the cart based on the ID of the item
-            //inside the cart
+            //filter method will be used here to filter the items in the cart based on the ID of the item inside the cart
             
             {
+                //we will be filtering based on ID i.e if the item.id is not equal to the action.payload.id then that particular item is removed from the cart
+
+                const removeItem = state.cart.filter((item)=> item.id !== action.payload.id)
+                state.cart = removeItem
+            },
+            incrementQuantity:(state, action) => {
+                // again we are gonna check if the item is already present in the cart
+                //this will be applied on the + button since the function is increment
+                const itemPresent = state.cart.find((item)=>item.id === action.payload.id);
+                itemPresent.quantity++;
+            },
+            decrementQuantity:(state,action)=>{
+                //we are gonna again check if the item is already present so decrement quantity is applied on the
+                //minus button *
+                const itemPresent = state.cart.find((item)=>item.id === action.payload.id);
+                if(itemPresent.quantity === 1) {
+                    itemPresent.quantity = 0;
+                    const removeItem = state.cart.filter((item)=> item.id !== action.payload.id)
+                    state.cart = removeItem;
+                }
+                else{
+                        //quantity is more than 1
+                        itemPresent.quantity--;
+                    }
+                }
 
             }
-}
+
 })
+
+
+
 
 //if the user presses on the addtocart button for the first time
 //then we need to push those particular details into the cart like
@@ -37,3 +64,7 @@ export const CartSlice = createSlice({
 //so if the item is already present inside the cart that means if the
 // quantity is 1 then in that case we are just going to increase the
 // quanity of that particular item by 1
+
+
+//* so if the quantity of that particular item is one & if the user presses presses on the minus button then we should just remove that item from the cart and we should again show this Add Button UI again.
+//So in case if the quantity of that particular item is more than 1 like 2,5,10 then in that case if the user presses on the minus button the quantity should be decreased by 1 only
